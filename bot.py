11d103d850419af349dad45bfa99fe225d935aa8
yhlee09 <85337498+yhlee09@users.noreply.github.com>
2021-07-11 21:20:41 +0900

    Update bot.py
    python의 discord 구문 변화

--- bot.py
+++ bot.py
@@ -1,21 +1,28 @@
+import asyncio
 import discord
-from discord.ext import commands
-import os
 
-client = commands.Bot(command_prefix = '-')
+client = discord.Client()
 
+# 생성된 토큰을 입력해준다.
+token = "토큰"
+
+# 봇이 구동되었을 때 보여지는 코드
 @client.event
 async def on_ready():
+    print("다음으로 로그인합니다")
+    print(client.user.name)
+    print(client.user.id)
+    print("================")
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
-  await client.change_presence(status=discord.Status.online)
+# 봇이 특정 메세지를 받고 인식하는 코드
+@client.event
+async def on_message(message):
+    # 메세지를 보낸 사람이 봇일 경우 무시한다
+    if message.author.bot:
+        return None
 
-  await client.change_presence(activity=discord.Game(name="게임 하는중"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
-  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+    if message.content.startswith('!안녕'):
+        channel = message.channel
+        await channel.send('반가워!')
 
-
-client.run(os.environ['token'])
+client.run(token)
